7603bd2eb4bbf066fa12f93e441d7e2851dab933
Viren Bhanushali <viren@openexcell.com>
2022-07-25 17:20:25 +0530

    update port for heroku

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ mongoose
   .connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
     console.log("MongoDB Connected");
-    return server.listen();
+    return server.listen(process.env.PORT || 5000);
   })
   .then((res) => {
     console.log(`Server running at ${res.url}`);
